b3fca675ef1ed1b408e842c893e09cae1f4c7b29
조장우 <jopuk2@gmail.com>
2020-07-02 21:52:14 +0900

    복귀한 화면에서 상황을 따져서 데이터 반영  - onActivityResult 함수 오버라이딩

--- app/src/main/java/kr/co/tjoeun/intent_20200702/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/intent_20200702/MainActivity.kt
@@ -1,5 +1,6 @@
 package kr.co.tjoeun.intent_20200702
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -47,5 +48,29 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
 
         }
+    }
+
+// 결과를 가지고 돌아올때 실행되는 함수
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+//        어떤 결과를 가지러 다녀온건지 확인.
+
+        if (requestCode == 1000) {
+//            완료로 돌아온게 맞는지 확인
+            if (resultCode == Activity.RESULT_OK){
+
+                val newNickname  = data?.getStringExtra("nickName")
+
+//                텍스트뷰에 반영
+
+                nickNameTxt.text = newNickname
+            }
+
+        }
+
+
+
     }
 }
